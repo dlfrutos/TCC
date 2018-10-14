d7f6a3be4e45b343ed3dbf2da9f8c2a5fc7cf1b6
Daniel <dlfrutos@hotmail.com>
2018-10-14 12:29:11 -0300

    up

--- app-android/AppBusaoV2/app/src/main/java/com/example/daniel/jsontest2/Activities/1_3.LinhaPontoHoraActivity.kt
+++ app-android/AppBusaoV2/app/src/main/java/com/example/daniel/jsontest2/Activities/1_3.LinhaPontoHoraActivity.kt
@@ -12,12 +12,11 @@ import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_lhp.*
 import okhttp3.*
 import java.io.IOException
+import java.util.*
 
 class LinhaPontoHoraActivity : AppCompatActivity() {
     lateinit var pontoSelecionado: String
     lateinit var linhaSelecionada: String
-    //lateinit var pontosFeed: PontosFeed
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +38,6 @@ class LinhaPontoHoraActivity : AppCompatActivity() {
         val url = "https://raw.githubusercontent.com/dlfrutos/TCC/master/Repositorio/ModelagemDB/ListaPontosLinhas.json"
         val request = Request.Builder().url(url).build()
         val client = OkHttpClient()
-
 
         client.newCall(request).enqueue(object : Callback {
 
@@ -74,15 +72,92 @@ class LinhaPontoHoraActivity : AppCompatActivity() {
             }
 
             private fun encontraHora(pontosFeed: PontosFeed?): PontosFeed? {
-                var i =0
-                //deleta todas as linhas != da selecionada
-                while (i<pontosFeed!!.horaLinha.count()){
-                    if(pontosFeed.horaLinha.get(i).LinhaID != linhaSelecionada){
+                var i = 0
+                val calendario = Calendar.getInstance()
+                val horaCalculada = calendario
+                val intervaloMinutos = 10
+
+                //
+                horaCalculada.set(calendario.get(Calendar.YEAR), calendario.get(Calendar.MONTH), calendario.get(Calendar.DATE), 0, 0, 0)
+
+//                val diaSemana = Calendar.getInstance().get(Calendar.DAY_OF_WEEK) //dia da semana int
+//                val diaMes = Calendar.getInstance().get(Calendar.DATE)  //dia do mes int
+//                val horaDia = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) //hora atual int
+//                val minutoDia = Calendar.getInstance().get(Calendar.MINUTE) //hora atual int
+//                val teste2 = calendario
+//                val teste3 = calendario
+
+                //gera uma data
+//                teste3.set(calendario.get(Calendar.YEAR),calendario.get(Calendar.MONTH),calendario.get(Calendar.DATE), 0,0,0)
+//                println("teste3: "+teste3.time)
+
+                //ADIÇÃO DE INTERVALO
+//                println("teste: "+calendario.time)
+//                teste2.add(Calendar.MINUTE, 5)
+
+                //              println("teste2: "+teste2)
+
+                //deleta todas as linhas != daquela selecionada
+                while (i < pontosFeed!!.horaLinha.count() - 1) {
+                    //filtra linha selecionada
+                    if (pontosFeed.horaLinha.get(i).LinhaID != linhaSelecionada) {
                         pontosFeed.horaLinha.removeAt(i)
+                        i--
+                    }
+                    //filtra dia semana
+                    else if (pontosFeed.horaLinha.get(i).DiaSemana.contains((calendario.get(Calendar.DAY_OF_WEEK)).toString())) {
+                        //faz nada
+                    } else {
+                        //remove o ponto pelo dia da semana
+                        pontosFeed.horaLinha.removeAt(i)
+                        i--
+                    }
+
+                    //aumenta index i
+                    i++
+                }
+
+                //deleta todos os pontos que não competem a linha selecionada
+                i = 0
+                while (i < pontosFeed.horaLinhaPontos.count() - 1) {
+                    if (pontosFeed.horaLinhaPontos.get(i).LinhaID != linhaSelecionada) {
+                        println("Removido:" + i + " (" + pontosFeed.horaLinhaPontos.get(i).LinhaID + ")")
+                        pontosFeed.horaLinhaPontos.removeAt(i)
                         i--
                     }
                     i++
                 }
+
+
+                //populando com horarios os ponstos da linha selecionada
+                // i= hora inicial da linha
+                // j= ponto inicial da linha
+                i = 0
+                while (i < pontosFeed.horaLinha.count() - 1) {
+                    var j = 0
+                    var minuto: Int
+                    var hora: Int
+                    //varredura dos pontos da linha
+                    while (j < pontosFeed.horaLinhaPontos.count()) {
+
+                        val minuto = pontosFeed.horaLinha.get(i).Hora.substring(0, 2)
+                        val hora = pontosFeed.horaLinha.get(i).Hora.substring(pontosFeed.horaLinha.get(i).Hora.length - 2, pontosFeed.horaLinha.get(i).Hora.length)
+                        //seta a hora do primeiro ponto (programada)
+                        if (j == 0) {
+                        }
+
+                        //caso nao seja o primeiro calcula pelo intervalo
+                        else {
+                            pontosFeed.horaLinhaPontos.get(j).Hora += intervaloMinutos
+                        }
+
+                        if (pontosFeed.horaLinhaPontos.get(j).PontoID == pontoSelecionado) {
+                        }
+                        j++
+                    }
+                    i++
+                }
+
                 return pontosFeed
             }
         })
